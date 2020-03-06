4e5ca9f620a9c0ac7da22d6c2820ebf047ce7d92
Smile Gupta <guptamiley3012@gmail.com>
2020-03-06 16:46:52 +0530

    Update Diameter of Binary Tree.cpp

--- Leet-Code/Diameter of Binary Tree.cpp
+++ Leet-Code/Diameter of Binary Tree.cpp
@@ -1,3 +1,27 @@
 //Link to the question: https://leetcode.com/problems/diameter-of-binary-tree/
 
 //Solution:
+int ans=0;
+    int fun(TreeNode* root){
+        if(root==NULL)
+            return 0;
+    if(root->left==NULL && root->right==NULL)
+    {
+        ans=max(1,ans);
+        return 1;
+    }
+        
+    int left=fun(root->left);
+    int right=fun(root->right);
+        
+    ans=max(1+left+right, ans);
+    return max(left,right)+1;
+        
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        ans=0;
+        fun(root);
+        return max(0,ans-1);
+        return 0;
+    }
+    
