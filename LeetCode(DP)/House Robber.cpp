1c2df9262718a09ad3e1aab429e028a08e6166db
Smile Gupta <guptamiley3012@gmail.com>
2020-03-08 14:28:48 +0530

    Update House Robber.cpp

--- LeetCode(DP)/House Robber.cpp
+++ LeetCode(DP)/House Robber.cpp
@@ -1,6 +1,6 @@
 //Link to the problem: https://leetcode.com/problems/house-robber
 
-//Solution:
+//Solution: Using Top Down Approch
 class Solution {
 public:
     int dp[100005];
@@ -25,5 +25,26 @@ public:
         fun(nums,0,n);
         
         return max(dp[0],0);
+    }
+};
+
+//Solution: Bottom Up Approch
+
+class Solution {
+public:
+   
+    int rob(vector<int>& nums) {
+       int n=nums.size();
+        if(n==0)
+        return 0;
+        int dp[n][2];
+        dp[0][0]=0;
+        dp[0][1]=nums[0];
+        for(int i=1; i<n; i++)
+        {
+            dp[i][0]=max(dp[i-1][0], dp[i-1][1]);
+            dp[i][1]=dp[i-1][0]+nums[i];
+        }
+        return max(dp[n-1][0], dp[n-1][1]);
     }
 };
